2347904ff3cfa8a4cc1ab566e5f19af1a211a8c3
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-08 13:13:44 -0600

    Cookie Verification

--- Program.cs
+++ Program.cs
@@ -15,10 +15,21 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+// MFA Services
 builder.Services.AddAuthentication().AddGoogle(googleOptions =>
 {
     googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
+// Cookie consent notification
+builder.Services.Configure<CookiePolicyOptions>(options => {
+    // This lambda determines whether user consent for non-essential 
+    // cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.ConsentCookieValue = "true";
 });
 
 var app = builder.Build();
@@ -35,6 +46,7 @@ else {
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy(); // Also added for cookie notification
 
 app.UseRouting();
 
