0553486939eb8b82bb691d2c330c97346baff7ab
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-08 18:17:30 -0600

    Integrated second database with sales tables

---
+++ Models/LineItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEXII.Models;
+
+public partial class LineItem
+{
+    public int TransactionId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int Qty { get; set; }
+
+    public int Rating { get; set; }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEXII.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Year { get; set; }
+
+    public decimal NumParts { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string? ImgLink { get; set; }
+
+    public string? PrimaryColor { get; set; }
+
+    public string? SecondaryColor { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Category { get; set; }
+}
