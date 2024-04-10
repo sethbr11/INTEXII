ee1e2f9997d8be107b4c05935a58dd99d09eba62
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-10 16:29:58 -0600

    CSP Header

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Humanizer;
 using INTEXII.Data;
 using INTEXII.Models;
 using Microsoft.AspNetCore.Identity;
@@ -23,14 +24,15 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-// MFA Services
+// MFA Services. See here: https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/?view=aspnetcore-8.0&tabs=visual-studio
+// Google: https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-8.0
 builder.Services.AddAuthentication().AddGoogle(googleOptions =>
 {
     googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
 });
 
-// Cookie consent notification
+// Cookie consent notification. See here: https://learn.microsoft.com/en-us/aspnet/core/security/gdpr?view=aspnetcore-8.0
 builder.Services.Configure<CookiePolicyOptions>(options => {
     // This lambda determines whether user consent for non-essential 
     // cookies is needed for a given request.
@@ -40,7 +42,7 @@ builder.Services.Configure<CookiePolicyOptions>(options => {
     options.ConsentCookieValue = "true";
 });
 
-// Stronger password requirements
+// Stronger password requirements. See here: https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity-configuration?view=aspnetcore-8.0
 builder.Services.Configure<IdentityOptions>(options => {
     // Default Password settings.
     options.Password.RequireDigit = true;
@@ -60,6 +62,16 @@ builder.Services.AddSession();
 
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // required to access the current session in the SessionCart class
+
+// HSTS Header
+// Check out this site: https://www.stackhawk.com/blog/net-http-strict-transport-security-guide-what-it-is-and-how-to-enable-it/
+// Maybe look into this one too? https://www.hanselman.com/blog/how-to-enable-http-strict-transport-security-hsts-in-iis7
+builder.Services.AddHsts(options => {
+    options.Preload = true; // Preloading means adding your website to the HSTS preload list, which is maintained by web browsers. Being on this list ensures that browsers will always use HTTPS for your website, even for the first visit.
+    options.IncludeSubDomains = true; // This instructs browsers to apply the HSTS policy not only to your main domain but also to all of its subdomains.
+    options.MaxAge = TimeSpan.FromDays(60); // It specifies how long (in seconds) the HSTS policy should be cached by the browser. In this case, it's set to 60 days.
+    // options.ExcludedHosts.Add("example.com"); // If you have subdomains that you don't want to include in the HSTS policy, you can add them to the ExcludedHosts list.
+});
 
 var app = builder.Build();
 
@@ -83,6 +95,20 @@ app.UseSession(); // Use the session that we set up above
 app.UseRouting();
 
 app.UseAuthorization();
+
+// CSP Header -- NEEDS TO BE CONFIGURED CORRECTLY
+// See here: https://www.stackhawk.com/blog/net-content-security-policy-guide-what-it-is-and-how-to-enable-it/
+app.Use(async (context, next) => {
+    context.Response.Headers.Add("Content-Security-Policy",
+        "default-src 'self'; " +
+        "script-src 'self'; " +
+        "style-src 'self'; " +
+        "font-src 'self'; " +
+        "img-src 'self'; " +
+        "frame-src 'self' http://localhost:53172;");
+
+    await next();
+});
 
 // Default routing
 app.MapControllerRoute(
