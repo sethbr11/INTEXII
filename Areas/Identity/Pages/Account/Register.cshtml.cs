f016c9d52d37242bd162d18406423bbe04a8113b
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-10 19:50:12 -0600

    Created admin role

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -21,6 +21,7 @@ using Microsoft.Extensions.Logging;
 
 namespace INTEXII.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
