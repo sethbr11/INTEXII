86bc7fab09b609a5e610641d6e74f0a61ebf4389
Seth Brock <144707380+sethbr11@users.noreply.github.com>
2024-04-11 23:03:57 -0600

    Scaffolded the ASP Users

---
+++ Models/AspNetUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEXII.Models;
+
+public partial class AspNetUser
+{
+    public string Id { get; set; } = null!;
+
+    public string? UserName { get; set; }
+
+    public string? NormalizedUserName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? NormalizedEmail { get; set; }
+
+    public int EmailConfirmed { get; set; }
+
+    public string? PasswordHash { get; set; }
+
+    public string? SecurityStamp { get; set; }
+
+    public string? ConcurrencyStamp { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public int PhoneNumberConfirmed { get; set; }
+
+    public int TwoFactorEnabled { get; set; }
+
+    public string? LockoutEnd { get; set; }
+
+    public int LockoutEnabled { get; set; }
+
+    public int AccessFailedCount { get; set; }
+
+    public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; } = new List<AspNetUserClaim>();
+
+    public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; } = new List<AspNetUserLogin>();
+
+    public virtual ICollection<AspNetUserToken> AspNetUserTokens { get; set; } = new List<AspNetUserToken>();
+
+    public virtual ICollection<AspNetRole> Roles { get; set; } = new List<AspNetRole>();
+}
